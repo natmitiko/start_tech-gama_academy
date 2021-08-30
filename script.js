68caec4e1ba7c590d5cb679d04d5670c5796e177
unknown <natalia.aono@unesp.br>
2021-08-29 21:21:25 -0300

    Adicionado arquivos e atualizado html e css

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("javascript carregado");
+
+// function validacaoCPF(cpf) {}
+
+function envioDados() {
+    console.log("enviando dados");
+    var nome = document.getElementById("nome").value;
+    var cargo = document.getElementById("cargo").value;
+    var dia = document.getElementById("dia").value;
+    var mes = document.getElementById("mes").value;
+    var ano = document.getElementById("ano").value;    
+    var civil = document.getElementById("civil").value;
+    var sexo = document.getElementById("sexo").value;
+    var cep = document.getElementById("cep").value;
+    var logradouro = document.getElementById("logradouro").value;
+    var numero = document.getElementById("numero").value;
+    var complemento = document.getElementById("complemento").value;
+    var bairro = document.getElementById("bairro").value;
+    var cidade = document.getElementById("cidade").value;
+    var estado = document.getElementById("estado").value;
+    var celular = document.getElementById("celular").value;
+    var tel_res = document.getElementById("tel_res").value;
+    var tel_comer = document.getElementById("tel_comer").value;
+    var contato = document.getElementById("contato").value;
+    var email = document.getElementById("email").value;
+    var identidade = document.getElementById("identidade").value;
+    var cpf = document.getElementById("cpf").value;
+    var veiculo = document.getElementById("veiculo").value;
+    var cnh = document.getElementById("cnh").value;
+    
+    // var resultadoValidacaoCPF = validacaoCPF(cpf)
+
+}
